ce9a2d6bbc8df79039af71f9cc52e2c6675a6344
[Priyanshi Srivastava] <[srivastavapriyanshi8081@gmail.com]>
2025-05-02 17:06:28 +0530

    Your commit message here

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'To_Do App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,26 +23,15 @@ class MyApp extends StatelessWidget {
 class ToDoApp extends StatefulWidget {
   const ToDoApp({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-
-
   @override
-  State<ToDoApp> createState() => _ToDoAppState();
+  _ToDoAppState createState() => _ToDoAppState();
 }
 
-
 class _ToDoAppState extends State<ToDoApp> {
-  final List<String> _tasks = [];
-  final TextEditingController _controller = TextEditingController();
+  List<String> _tasks = []; // List to store tasks
+  final TextEditingController _controller = TextEditingController(); // Controller for the input field
 
+  // Method to add a task to the list
   void _addTask() {
     if (_controller.text.isNotEmpty) {
       setState(() {
@@ -68,6 +41,7 @@ class _ToDoAppState extends State<ToDoApp> {
     }
   }
 
+  // Method to remove a task from the list
   void _removeTask(int index) {
     setState(() {
       _tasks.removeAt(index);
@@ -76,32 +50,18 @@ class _ToDoAppState extends State<ToDoApp> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text('To-Do App'),
-        centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             TextField(
               controller: _controller,
               decoration: const InputDecoration(
                 labelText: 'Enter a task',
-                border: OutlineInputBorder(),
               ),
             ),
             const SizedBox(height: 10),
